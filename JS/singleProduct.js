7d9f11d589aef48ce2b57413dfb30b0ae111d7bb
phamtienduceng <phamtienduceng@gmail.com>
2022-12-11 21:56:58 +0700

    new

---
+++ JS/singleProduct.js
@@ -0,0 +1,4 @@
+function changeImage(fileName){
+    let img=document.querySelector("#changePic");
+    img.setAttribute("src", fileName);
+}
--- Product Detail/javaScript.js
+++
@@ -1,7 +0,0 @@
-function changeImage(this) {
-
-    var main_prodcut_image = document.getElementById('main_product_image');
-    main_prodcut_image.src = element.src;
-    
-
-}
